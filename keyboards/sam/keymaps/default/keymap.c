76c50f42f1b0070964383313ac63e1409b8370d4
Sam Garson <samtgarson@gmail.com>
2016-12-29 20:15:20 +0000

    Add second layer

--- keyboards/sam/keymaps/default/keymap.c
+++ keyboards/sam/keymaps/default/keymap.c
@@ -1,12 +1,21 @@
 #include "sam.h"
 
+#define ______ KC_TRNS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = KEYMAP( /* Base */
   KC_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPACE, \
   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_ENTER, KC__VOLUP, \
   KC_CAPSLOCK, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_BSLASH, KC__VOLDOWN, \
   KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_UP, KC__MUTE, \
-  KC_FIND, KC_LCTRL, KC_LALT, KC_LGUI, KC_SPACE, KC_RGUI, KC_RALT, KC_FIND, KC_FIND, KC_LEFT, KC_DOWN, KC_RIGHT \
+  MO(1), KC_LCTRL, KC_LALT, KC_LGUI, KC_SPACE, KC_RGUI, KC_RALT, KC_RCTRL, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT \
+),
+[1] = KEYMAP( /* Raise */
+  KC_SLEP, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
+  ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______,  ______,  ______,  ______,  ______,  KC_MNXT, \
+  ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______,  ______,  ______,  ______, KC_MPLY, \
+  ______,  KC_GRV, ______, ______, ______, ______, ______, ______, ______, ______,  ______,  ______, KC_PGUP, KC_MPRV, \
+  ______,  ______, ______, ______, ______, ______, ______, ______, ______, KC_HOME, KC_PGDN, KC_END \
 ),
 };
 
